998981d91da73f5fb9f5bbf5a66e56998fe99168
casaxmore <casaxmore@gmail.com>
2023-02-13 15:04:32 +0100

    Formulario reactivo terminado

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,10 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+/* import { emailPattern, nombreApellidoPattern, noPuedeSerStrider } from 'src/app/shared/validator/validaciones'; */
+import { ValidatorService } from '../../../shared/validator/validator.service';
+import { EmailValidatorService } from '../../../shared/validator/email-validator.service';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.css']
 })
-export class RegistroComponent {
+export class RegistroComponent implements OnInit{
+
+  constructor(private fb: FormBuilder, private validatorService: ValidatorService, private emailValidator: EmailValidatorService){}
+
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required, Validators.pattern(this.validatorService.nombreApellidoPattern)]],
+    email: ['', [Validators.required, Validators.pattern(this.validatorService.emailPattern)], [this.emailValidator]],
+    username: ['', [Validators.required, this.validatorService.noPuedeSerStrider]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]],
+  }, {
+    validators: [this.validatorService.camposIguales('password', 'password2')]
+  })
+
+  ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: 'casaXmore Jar',
+      email: 'casaxmore@gmail.com',
+      username: 'csx',
+      password: '123456',
+      password2: '123456'
+    })
+  }
+
+  get emailErrorMsg(): string {
+
+    const errors = this.miFormulario.get('email')?.errors;
+    if(errors?.['required']){
+      return 'Email es obligatorio'
+    }else if(errors?.['pattern']){
+      return 'El valor no tiene formato de correo'
+    }else if(errors?.['emailTomador']){
+      return 'Este correo electrónico ya existe'
+    }
+
+    return '';
+  }
+
+  campoNoValido(campo:string) {
+    return this.miFormulario.get(campo)?.invalid
+            && this.miFormulario.get(campo)?.touched;
+  }
+
+/*   emailRequired() {
+    return this.miFormulario.get('email')?.errors?.['required']
+            && this.miFormulario.get('email')?.touched;
+  }
+
+  emailFormato() {
+    return this.miFormulario.get('email')?.errors?.['pattern']
+            && this.miFormulario.get('email')?.touched;
+  }
+
+  emailTomado() {
+    return this.miFormulario.get('email')?.errors?.['emailTomador']
+            && this.miFormulario.get('email')?.touched;
+  } */
+
+  submitFormulario() {
+    console.log(this.miFormulario.value);
+    this.miFormulario.markAllAsTouched();
+  }
 
 }
